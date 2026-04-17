525c48105267920adfe079a26ef781199f96f355
Tom Soldner <tomsoldner4@gmail.com>
2026-04-17 16:32:32 -0400

    Updated Edonn Z6 texture and first pass at code for jenna's jetpacks

--- TAEGear/Backpacks/config.cpp
+++ TAEGear/Backpacks/config.cpp
@@ -31,4 +31,45 @@ class CfgVehicles {
 		descriptionShort = "Edonn's Z-6 Jetpack";
 		hiddenSelectionsTextures[] = {"TAEGear\data\Edonn_Z6_CO.paa"};
 	};
+	
+	
+	/*
+	//Jennas jumppacks test
+	class Bag_Base;
+	class tae_jetpack : Bag_Base {
+		scope = 1;
+		displayName = "TAE base";
+		picture = "\z\tgf\addons\backpacks\jt12\data\JT12.paa";
+		tas_is_jetpack = 0;
+		maximumload = 500;
+		jen_jetpacks_core_isJetpack = 1;
+		jen_jetpacks_core_acceleration = 2;
+		jen_jetpacks_core_drag = 6;
+		mass = 50;
+		jen_jetpacks_core_fuelCoef = 1;
+		jen_jetpacks_core_heatCoef = 1;
+		jen_jetpacks_core_coolCoef = 1;
+		jen_jetpacks_core_ascensionCoef = 1;
+		jen_jetpacks_core_jumpCoef = 1;
+		jen_jetpacks_core_fuelCapacity = 250;
+		jen_jetpacks_core_strafeCoef = 1;
+	};
+	class tae_hondo_JT12_LR : tae_jetpack {
+		scope = 2;
+		displayName = "Hondo's JT-12 Jetpack";
+		descriptionShort = "Hondo's JT-12 Jetpack";
+		model = "\z\tgf\addons\backpacks\jt12\jt12_jetpack.p3d";
+		hiddenSelectionsTextures[] = {"TAEGear\data\JT12_Hondo.paa"};
+		picture = "\z\tgf\addons\backpacks\jt12\data\JT12.paa";
+		tf_hasLRradio = 1;
+		tf_range = 13500;
+	};
+	class tae_edonn_J6 : tae_jetpack {
+		scope = 2;
+		model = "\z\tgf\addons\backpacks\z6\z6_jetpack.p3d";
+		displayName = "Edonn's Z-6 Jetpack";
+		descriptionShort = "Edonn's Z-6 Jetpack";
+		hiddenSelectionsTextures[] = {"TAEGear\data\Edonn_Z6_CO.paa"};
+		picture = "\z\tgf\addons\backpacks\z6\data\Z6.paa";
+	};*/
 };
